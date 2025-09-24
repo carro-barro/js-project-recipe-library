f14f796412c1df3568514eb76550e6af8e3425f7
Carolina Oldertz <carolina@Carolinas-MacBook-Air.local>
2025-09-24 14:48:31 +0200

    started slowly with the javascript, tried to figure it out on my own but it was a bit hard

---
+++ script.js
@@ -0,0 +1,28 @@
+const btnBreakfast = document.getElementById("btnBreakfast")
+const btnLunch = document.getElementById("btnLunch")
+const btnFika = document.getElementById("btnFika")
+const btnDinner = document.getElementById("btnDinner")
+
+const filterBreakfast = () => {
+// add the filtering that happens when breakfast btn is clicked
+}
+
+btnBreakfast.addEventListener("click", filterBreakfast)
+
+const filterLunch= () => {
+  // add the filtering that happens when Lunch btn is clicked
+}
+
+btnLunch.addEventListener("click", filterLunch)
+
+const filterFika = () => {
+  // add the filtering that happens when Fika btn is clicked
+}
+
+btnFika.addEventListener("click", filterFika)
+
+const filterDinner = () => {
+  // add the filtering that happens when Lunch btn is clicked
+}
+
+btnDinner.addEventListener("click", filterDinner)
